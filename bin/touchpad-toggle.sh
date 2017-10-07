59dc4ba61a5f9c02a496dee7f14cc8a7c7305f23
Radimir Mikhailov <radimir.com@gmail.com>
2017-10-07 19:07:12 +0700

    Сделаны отдельными скрипты включения и отключения тачпада

--- bin/touchpad-toggle.sh
+++ bin/touchpad-toggle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #-------------------------------------------------------------------------------
-# Настройка клавиатуры в X-сессии через подсистему xkb
-# ----------------------------------------------------
+# Включить/отключить тачпад
+# -------------------------
 #
 #-------------------------------------------------------------------------------
 DEVICE_ID=`xinput --list --short | grep -Po '.*Mouse.*id=\K(\d+)'`
@@ -9,7 +9,7 @@ STATUS_NOW=`xinput --list-props $DEVICE_ID | grep -Po 'Device Enabled.+:\s+\K(\d
 
 if [ "$STATUS_NOW" = "0" ]
 then
-    xinput --enable $DEVICE_ID
+    touchpad-on.sh
 else
-    xinput --disable $DEVICE_ID
+    touchpad-off.sh
 fi
